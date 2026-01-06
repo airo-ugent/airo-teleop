12b5191b4aba76346c32af8961a96c60db9d3b96
rproesma <remko.proesmans@ugent.be>
2026-01-06 16:05:41 +0100

    created Gello4UR base class for Gello4UR_ParallelGripper

--- airo-teleop-agents/airo_teleop_agents/gello_teleop_agents.py
+++ airo-teleop-agents/airo_teleop_agents/gello_teleop_agents.py
@@ -10,25 +10,21 @@ import numpy as np
 import ur_analytic_ik
 
 
-class Gello4UR_ParallelGripper(TeleopAgent):
+class Gello4UR(TeleopAgent):
     def __init__(self, gello_usb_port: str, 
                  gello_config: GelloConfig, 
                  ur_robot: URrtde,
-                 gripper: ParallelPositionGripper, 
                  use_joint_space: bool=True):
         """
         This class implements a teleoperation agent for any combination of Gello teleop device, UR robot, 
         and parallel gripper (e.g. Robotiq 2F-85 and Schunk EGK40).
         :param gello_usb_port: COM port to which the Gello device is connected
         :param ur_robot: airo_robots.manipulators.hardware.ur_rtde.URrtde object representing the UR robot to be teleoperated
-        :param gripper: airo_robots.grippers.hardware.parallel_position_gripper.ParallelPositionGripper object representing the gripper attached to the UR robot
         :param gello_config: Configuration for the Gello device
-        :param use_joint_space: Details whether to use joint space or tool space
+        :param use_joint_space: Whether to use joint space control or tool space control
         """       
         self.gello_usb_port = gello_usb_port
         self.ur_robot = ur_robot
-        self.gripper = gripper
-        self.gripper_opening_range = (self.gripper.gripper_specs.min_width, self.gripper.gripper_specs.max_width)
         self.gello_config = gello_config
         self.use_joint_space = use_joint_space
 
@@ -41,9 +37,6 @@ class Gello4UR_ParallelGripper(TeleopAgent):
         ur_start_joints = self.ur_robot.get_joint_configuration()
         self.gello_config.dynamixel_config.start_joints = ur_start_joints
         gello = GelloTeleopDevice(gello_config=self.gello_config, port=self.gello_usb_port)
-        if self.gello_config.gripper_config is None:
-            logger.warning("Gello gripper config not provided, calibrating gello trigger...")
-            gello.calibrate_trigger()
         return gello
 
     def _build_transform_func(self):
@@ -52,8 +45,7 @@ class Gello4UR_ParallelGripper(TeleopAgent):
             def transform_func(raw_data: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]: # type: ignore
                 teleop_action = raw_data.copy()
                 # teleop_action[6] is the gripper mapped to [0, 1], with 0 = fully open, 1 = fully closed
-                teleop_action[6] = self.gripper_opening_range[0] + (self.gripper_opening_range[1] - self.gripper_opening_range[0]) * (1 - teleop_action[6])
-                return teleop_action
+                return teleop_action[:6]
         else:
             forward_kinematics_dict = {
                 URrtde.URModels.UR3: ur_analytic_ik.ur3.forward_kinematics,
@@ -67,6 +59,53 @@ class Gello4UR_ParallelGripper(TeleopAgent):
                 joint_positions = teleop_action[:6]
                 ee_pose = fk_function(*joint_positions)
                 # teleop_action[6] is the gripper mapped to [0, 1], with 0 = fully open, 1 = fully closed
-                teleop_action[6] = self.gripper_opening_range[0] + (self.gripper_opening_range[1] - self.gripper_opening_range[0]) * (1 - teleop_action[6])
-                return (ee_pose, teleop_action[6])
+                return ee_pose
+        return transform_func
+
+
+class Gello4UR_ParallelGripper(Gello4UR):
+    def __init__(self, gello_usb_port: str, 
+                 gello_config: GelloConfig, 
+                 ur_robot: URrtde,
+                 gripper: ParallelPositionGripper, 
+                 use_joint_space: bool=True):
+        """
+        This class implements a teleoperation agent for any combination of Gello teleop device, UR robot, 
+        and parallel gripper (e.g. Robotiq 2F-85 and Schunk EGK40).
+        :param gello_usb_port: COM port to which the Gello device is connected
+        :param ur_robot: airo_robots.manipulators.hardware.ur_rtde.URrtde object representing the UR robot to be teleoperated
+        :param gripper: airo_robots.grippers.hardware.parallel_position_gripper.ParallelPositionGripper object representing the gripper attached to the UR robot
+        :param gello_config: Configuration for the Gello device
+        :param use_joint_space: Whether to use joint space control or tool space control
+        """       
+        
+        self.gripper = gripper
+        self.gripper_opening_range = (self.gripper.gripper_specs.min_width, self.gripper.gripper_specs.max_width)
+        super().__init__(gello_usb_port=gello_usb_port, 
+                         gello_config=gello_config, 
+                         ur_robot=ur_robot,
+                         use_joint_space=use_joint_space)
+
+    def _init_gello_device(self) -> GelloTeleopDevice:
+        gello = super()._init_gello_device()
+        if self.gello_config.gripper_config is None:
+            logger.warning("Gello gripper config not provided, calibrating gello trigger...")
+            gello.calibrate_trigger()
+        return gello
+
+    def _build_transform_func(self):
+        super_transform_func = super()._build_transform_func()
+        if self.use_joint_space:
+            def transform_func(raw_data: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]: # type: ignore
+                joint_action = super_transform_func(raw_data)
+                # Map gripper from [0, 1] to [max_width, min_width]
+                gripper_action = self.gripper_opening_range[0] + (self.gripper_opening_range[1] - self.gripper_opening_range[0]) * (1 - raw_data[6])
+                #concatenate joint action and gripper action
+                teleop_action = np.concatenate((joint_action, [gripper_action]))
+                return teleop_action
+        else:
+            def transform_func(raw_data: npt.NDArray[np.float64]) -> tuple[HomogeneousMatrixType, float]:
+                ee_pose = super_transform_func(raw_data)
+                gripper_action = self.gripper_opening_range[0] + (self.gripper_opening_range[1] - self.gripper_opening_range[0]) * (1 - raw_data[6])
+                return (ee_pose, gripper_action)
         return transform_func
